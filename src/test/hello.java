f1ab9f4bef2dae948d32c4c328926e0456fe9a0a
louis cai <1290103192@qq.com>
2017-11-21 16:59:40 +0800

    test run 2
    request from desktop

--- src/test/hello.java
+++ src/test/hello.java
@@ -3,5 +3,11 @@ package test;
 public class hello {
 	public static void main(String[] args){
 		System.out.print("hello");
+		
+		
+		boolean f = true;
+		if(f){
+			System.out.print(", connection succeed!");
+		}
 	}
 }
